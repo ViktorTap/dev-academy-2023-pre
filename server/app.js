b60f8059ea163f3e12814099081a9442ed8e9084
ViktorTap <victor.tapdygov@gmail.com>
2023-04-05 16:55:58 +0300

    react project started, server urls works fine

--- server/app.js
+++ server/app.js
@@ -1,12 +1,65 @@
 import express from "express";
-import { getJourneys } from "./db.js";
+import { pool } from "./db.js";
 
 const app = express();
 
 app.get("/journeys", async (req, res) => {
-    const journeys = await getJourneys()
-    res.send(journeys)
+
+    try {
+        const { month, page, limit } = req.query;
+        
+        const offset = (page - 1) * limit;
+
+        const [data] = await pool.query("SELECT * FROM ?? WHERE duration > 10 OR coveredDistance > 10 LIMIT ? offset ?", [month, +limit, +offset]);
+
+        const [totalPageData] = await pool.query("SELECT COUNT(*) AS count FROM ??", [month]);
+
+        const totalPage = Math.ceil(+totalPageData[0]?.count / limit);
+
+        console.log("Total pages", totalPage, "of", month);
+
+        res.json({
+            data: data,
+            pagination: {
+                page: +page,
+                limit: +limit,
+                totalPage
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 })
+
+app.get("/stations", async (req, res) => {
+    try {
+        const { month, page, limit } = req.query;
+        
+        const offset = (page - 1) * limit;
+
+        const [data] = await pool.query("SELECT DISTINCT DepartureStationID, DepartureStationName FROM ?? ORDER BY DepartureStationID LIMIT ? OFFSET ?", [month, +limit, +offset])
+
+        const [totalPageData] = await pool.query("SELECT COUNT(DISTINCT DepartureStationID) AS count FROM ??", [month]);
+
+        const totalPage = Math.ceil(+totalPageData[0]?.count / limit);
+
+        console.log("Total pages", totalPage, "of", month);
+
+        res.json({
+            data: data,
+            pagination: {
+                page: +page,
+                limit: +limit,
+                totalPage
+            }
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
 
 app.use((err, req, res, next) => {
     console.log(err.stack);
